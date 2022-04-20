020e99cce35ddf2e4db10618c4a9653e313e2323
Massimiliano Puddu <massypuddu1985@libero.it>
2022-04-20 22:57:30 +0200

    add commentModel

---
+++ FrontEnd-Angular/src/app/models/movieCommentModel.ts
@@ -0,0 +1,9 @@
+export interface MovieCommentInterface{
+    title: string,
+    comment: string,
+    movieId: number,
+    commentId: number
+  }
+
+
+  
---
+++ FrontEnd-Angular/src/app/services/comments/comments.service.ts
@@ -0,0 +1,54 @@
+
+import { CommentsInterface} from 'src/app/models/commentModel'; 
+
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentsService {
+
+
+  private baseURL = 'http://localhost:5293/comments'; //variabile con indirizzo principale chiamata per comemnti
+
+  constructor(private http: HttpClient) { }
+
+  
+  getComments() {
+    return this.http.get<any>(this.baseURL);
+  }
+
+  getComment(id:number) {
+    return this.http.get<any>(this.baseURL + "/" + id);
+  }
+
+  getUserComments(id: number) {
+    return this.http.get<any>(this.baseURL + "?user-id=" + id);
+  }
+  
+  addComment = (userId: number, movieId: number, data: CommentsInterface) => {
+    return this.http.post<CommentsInterface>(this.baseURL, {
+      "userId": userId,
+      "movieId": movieId,
+      "body": data.body
+    }); 
+  }
+ 
+  deleteComment(id : number ) {
+    return this.http.delete(this.baseURL + "/" + id);
+  }
+ 
+  editComment = (data: CommentsInterface) => {
+    return this.http.put(this.baseURL + '/' + data.id, {
+      "id": data.id,
+      "userId": data.userId,
+      "movieId": data.movieId,
+      "body": data.body
+    });
+  };
+}
